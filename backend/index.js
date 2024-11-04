ecd054ce62993d8f1a6f6f3b77c6e5f12459c091
STMT <elahep113@naver.com>
2024-11-04 17:02:33 +0900

    jQuery CDN, mvp css 적용

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,39 @@
 const express = require('express')
 const app = express()
-const port = 3000 // process.env.PORT || 3000
+const port = process.env.PORT || 3000 // process.env.PORT || 3000
+const cors = require("cors");
 
 /*http 요청 : get, post, put, delete, (patch) */
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({}))
 let todos = [
   {
     userId: 1,
     id: 1,
     title: " deelectus aut autem",
+    date: new Date("2024-11-01"),
     completed: false
   },
   {
     userId: 1,
     id: 2,
     title: "gone with wind",
+    date: new Date("2024-11-01"),
     completed: false
   },
   {
     userId: 1,
     id: 3,
     title: " the perfect espresso",
+    date: new Date("2024-11-01"),
     completed: false
   },
 ];
+app.get("/todos", (req, res) => {
+  res.status(200).json(todos);
+});
 
 app.get("/todos/:id", (req, res) => {
   // 파라미터 변수 : 문자로 들어옴
