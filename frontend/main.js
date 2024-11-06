77337594d49967bf5bfcc50063e3c280eb2d5f24
STMT <elahep113@naver.com>
2024-11-06 17:06:15 +0900

    mvp semantic tag 적용

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,7 @@ $(function () {
         method: "get",
         success: function (rows) {
             const todoUL = $("#todos");
-            let str;
+            let str = ""; //변수가 초기화 되어서 정의되지 않은 상태가 됨.
             rows.forEach((row) => {
                 str += `<li class="d-flex gap-1">
                     <input type = "checkbox" ${row.completed ? "checked" : ""}>
@@ -15,4 +15,4 @@ $(function () {
             todoUL.html(str);
         },
     });
-})
+});
