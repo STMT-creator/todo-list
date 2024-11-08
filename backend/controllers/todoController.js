78b17f391b31617a80a4fbf4b7a97c4f53aaf293
STMT <elahep113@naver.com>
2024-11-08 16:15:48 +0900

    todos controller finished

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,12 +1,28 @@
 const Todo = require("../models/todoModel")
 
 const getALLTodos = async (req, res) => {
-    res.status(200).json("todos");
+    let todos;
+    try {
+        todos = await Todo.find();
+        if (!todos) {
+            throw new Error("등록된 할일이 없습니다.")
+            res.status(501).send("할일이 등록되어 있지 않습니다.")
+        }
+    } catch (error) {
+        console.log("getALLTodos Fail : ", error)
+    }
+    res.status(200).json(todos);
 }
 
 const getTodo = async (req, res) => {
-    const foundOne = todos.find(todo => todo.id === parseInt(req.params.id))
-    res.status(200).json(foundOne);
+    const id = req.params.id;
+    let todo;
+    try {
+        todo = await Todo.findById(id)
+    } catch (error) {
+        console.log("getTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
 }
 
 const addTodos = async (req, res) => {
@@ -23,18 +39,29 @@ const addTodos = async (req, res) => {
 }
 
 const updateTodo = async (req, res) => {
-    const { id, title, completed } = req.body;
-    const foundOne = todos.find(todo => todo.id === id);
-    foundOne.title = title;
-    foundOne.completed = completed;
-    res.status(200).json(todos);
+    const id = req.params.id;
+    const { title, completed } = req.body;
+    let todo;
+    try {
+        todo = await Todo.findByIdAndUpdate(id, {
+            title,
+            completed
+        });
+    } catch (error) {
+        console.log("updateTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
 }
 
 const removeTodo = async (req, res) => {
-    let { id } = req.body;
-    console.log(id)
-    todos.splice(--id, 1);
-    res.status(200).json(todos);
+    const id = req.params.id;
+    let todo;
+    try {
+        todo = await Todo.deleteOne({_id: id})
+    } catch (error) {
+        console.log("deleteTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
 }
 
 module.exports = { getALLTodos, getTodo, addTodos, updateTodo, removeTodo };
