33d0eb47335054e21209962365513362db085614
STMT <elahep113@naver.com>
2024-11-08 14:20:23 +0900

    let 지정 삭제

--- backend/routes/todoRouter.js
+++ backend/routes/todoRouter.js
@@ -14,27 +14,3 @@ router
     .delete(removeTodo);
 
 module.exports = router; //다른 위치의 js파일에서 router 객체를 불러올수 있음.
-
-// let todos = [
-//     {
-//         userId: 1,
-//         id: 1,
-//         title: "deelectus aut autem",
-//         date: new Date("2024-11-01"),
-//         completed: false
-//     },
-//     {
-//         userId: 1,
-//         id: 2,
-//         title: "gone with wind",
-//         date: new Date("2024-11-01"),
-//         completed: false
-//     },
-//     {
-//         userId: 1,
-//         id: 3,
-//         title: "the perfect espresso",
-//         date: new Date("2024-11-01"),
-//         completed: true
-//     },
-// ];
