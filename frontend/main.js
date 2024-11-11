980723343cd6a7170fd2087ea702589ef8a79135
STMT <elahep113@naver.com>
2024-11-11 17:28:12 +0900

    id를 통한 삭제 기능 구현

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,7 @@ function createTodo() {
     todosForm.addEventListener("submit", function () {
         // e.preventDefault();
         $.ajax({
-            url: "http://localhost:3000/todos",
+            url: 'http://localhost:3000/todos',
             method: "post",
             headers: {
                 "Content-Type": "application/json"
@@ -27,9 +27,9 @@ function createTodo() {
     })
 }
 
-function clearInputAndFocus(){
+function clearInputAndFocus() {
     todosForm.querySelector("#todo-text").value = "";
-todosForm.querySelector("#todo-text").focus();
+    todosForm.querySelector("#todo-text").focus();
 }
 async function fetchTodos() {
     $.ajax({
@@ -39,13 +39,32 @@ async function fetchTodos() {
             const todoUL = $("#todos");
             let str = ""; //변수가 초기화 되어서 정의되지 않은 상태가 됨.
             rows.forEach((row) => {
-                str += `<li class="d-flex gap-1">
+                str += `<li class="d-flex gap-1" data-id="${row._id}">
                     <input type = "checkbox" ${row.completed ? "checked" : ""}>
    <p>${row.title}</p>
-   <input type="button" value="modify">
-   <input type="button" value="remove">
+   <input type="button" value="수정" class="modify-btn">
+   <input type="button" value="삭제" class="remove-btn">
 </li>`});
             todoUL.html(str);
+            removeTodo();
         },
     });
 }
+
+function removeTodo() {
+    const removeBtns = $(".remove-btn");
+    removeBtns.on("click", function(){
+        const currBtn = $(this); // 현재 누른 버튼이 무엇인지
+        $.ajax({
+            url: `http://localhost:3000/todos/${currBtn.parent().attr("data-id")}`,
+            method: "delete",
+            success: function() {
+                alert("선택항목을 삭제하였습니다.")
+            }
+        }).done(() => {
+            fetchTodos();
+        })
+    })
+}
+
+//currBtn.parent().attr("data-id")
