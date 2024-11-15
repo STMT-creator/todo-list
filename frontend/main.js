164f0f94e5bfdb74e1b9a443808829c685455cba
STMT <elahep113@naver.com>
2024-11-15 18:16:30 +0900

    update | front 요청

--- frontend/main.js
+++ frontend/main.js
@@ -41,35 +41,64 @@ async function fetchTodos() {
         method: "get",
         success: function (rows) {
             const todoUL = $("#todos");
-            let str = ""; //변수가 초기화 되어서 정의되지 않은 상태가 됨.
+            let strArr = []; //변수가 초기화 되어서 정의되지 않은 상태가 됨.
             rows.forEach((row) => {
-                str += `<li class="d-flex gap-1" data-id="${row._id}">
+                strArr.push(`<li class="d-flex gap-1" data-id="${row._id}">
                     <input type = "checkbox" ${row.completed ? "checked" : ""}>
    <p>${row.title}</p>
    <input type="button" value="수정" class="modify-btn">
    <input type="button" value="삭제" class="remove-btn">
-</li>`});
-            todoUL.html(str);
+</li>`)
+});
+            strArr.reverse();
+            strArr.join("")
+            todoUL.html(strArr);
             removeTodo();
             callModal();
         },
     });
 }
+
 function callModal() {
     const editTodo = $(".modify-btn");
+    let todoId; // global var 전역으로 꺼내야만 인식을 한다.
     editTodo.on("click", function () {
         const edit = $(this);
-        const todoId = edit.parent().attr("date-id");
+        todoId = edit.parent().attr("data-id");
         const todoTitle = edit.siblings("p").text();
         const modal = $("#modal");
         modal.find("#prev-todo").val(todoTitle);
         modal.fadeIn("fast");
         modal.find("#next-todo").focus();
     });
-    $("#hide-modal").on("click", function(){
+    $("#edit-form").on("submit", function () {
+        // e.preventDefault();
+        if ($("#next-todo").val() == "") {
+            alert("변경할 내용을 입력해 주세요.")
+            $("#next-todo").focus();
+            return;
+        }
+        $.ajax({
+            url: `http://localhost:3000/todos/${todoId}`,
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                title: $("#next-todo").val(),
+                completed: $("#next-complete").is(":checked"),
+            }),
+            success: function () {
+                alert("항목이 업데이트 되었습니다.");
+                location.reload();
+            }
+        })
+    })
+    $("#hide-modal").on("click", function () {
         $("#modal").fadeOut("fast")
     })
 }
+
 function removeTodo() {
     const removeBtns = $(".remove-btn");
     removeBtns.on("click", function () {
